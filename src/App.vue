ac28a6d97a7fe4b391bd691b990338e37b7982d3
provenceblue <provenceblue1498@gmail.com>
2022-01-13 17:38:57 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
-
+  
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
 
 export default {
-  name: 'App',
+  data: function(){
+    return{
+      todoItems:[]
+    }
+  },
+  methods:{
+    addOneItem:function(todoItem){
+      var obj = {completed:false,item:todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+
+    },
+    removeOneItem:function(todoItem, index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index,1);
+    },
+    toggleOneItem:function(todoItem, index){
+      this.todoItems[index].completed = !this.todoItems[index].completed
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+
+    }
+  },
+  created:function(){
+        if(localStorage.length > 0){
+            for(var i=0;i<localStorage.length; i++){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    //console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+
+                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        }
+    },
   components: {
-    HelloWorld
-  }
+    'TodoHeader': TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter
+  },
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+body{text-align:center;background-color:#f6f6f6;}
+input{border-style:groove;width:200px;}
+.shadow{box-shadow:5px 10px 10px rgba(0,0,0,0.03);}
 </style>
