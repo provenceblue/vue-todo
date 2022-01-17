b19ed4a4ed8caa1ea6449d64e64356a8d0182e4e
pvb <provenceblue1498@gmail.com>
2022-01-17 15:57:48 +0900

    Revert "마스터 머지"
    This reverts commit de86f954a7f03d1f34d499c914f4c92d9be67366.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ul>
+        <transition-group name="list" tag="ul">
             <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
                 <i class="checkBtn fas fa-check" v-bind:class="{checkBtnComplete:todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
                 <span v-bind:class="{textCompleted:todoItem.completed}">{{todoItem.item}}</span>
@@ -8,7 +8,7 @@
                     <i class="fas fa-trash-alt"></i> 
                 </span>
             </li>
-        </ul>
+        </transition-group>
     </div>
 </template>
 
@@ -35,4 +35,12 @@ li{display:flex;min-height:50px;height:50px;line-height:50px;margin:0.5rem 0;pad
 .checkBtnComplete{color:#b3adad;}
 .textCompleted{text-decoration:line-through;color:#b3adad;}
 .removeBtn{margin-left:auto;color:#de4343;}
+
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
 </style>
