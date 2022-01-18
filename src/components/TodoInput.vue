0be5bfa6f2f47374e56a3df57f7d616fd340e39c
ekkim <provenceblue1498@gmail.com>
2022-01-18 17:38:13 +0900

    메소드 함수명 줄이기, Modal = Modal --> Modal 로 줄이기

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -17,14 +17,14 @@
 import Modal from './common/modal.vue'
 
 export default {
-    data:function(){
+    data(){
         return{
             newTodoItem:"",
             showModal : false
         }
     },
     methods:{
-        addTodo: function(){
+        addTodo(){
             if(this.newTodoItem !== ''){
                 this.$emit('addTodoItem', this.newTodoItem)
                 this.clearInput();
@@ -33,12 +33,12 @@ export default {
             }
             
         },
-        clearInput: function(){
+        clearInput(){
             this.newTodoItem="";
         }
     },
     components:{
-        'Modal' : Modal
+        Modal
     }
     
 };
